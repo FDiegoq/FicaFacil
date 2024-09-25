ce04d3b67354a8e47b60d0001828b1cc442576cb
Diego <fdiegofreire@gmail.com>
2024-09-25 15:38:49 -0300

    criação dos models relacionais

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUserUser
+from ..tasks.models import Empresa
 # Create your models here.
+
+class Usuario(AbstractUserUser):
+
+    #CAMPOS EXTRAS
+
+    Email=models.EmailField(blank=False, null=False)
+    Empresa=models.ForeignKey(Empresa,on_delete=models.CASCADE)
