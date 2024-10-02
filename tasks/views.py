ce8de822325349cdda58d92edd7914c1cc484358
Diego <fdiegofreire@gmail.com>
2024-10-01 21:17:03 -0300

    está renderizando tarefas por filtro

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from tasks.models import Tarefa
+from users.models import User
 # Create your views here.
 
 @login_required(login_url='login')
 def home(request):
-    tasks=Tarefa.objects.all()
+    user_logado=request.user
+    tasks = Tarefa.objects.filter(usuario=user_logado) ##Aqui eu estou retornando uma lista de tarefas filtrando por usuário, apenas pegando tarefas do usuário que está logado, com o dado da request.user
     context={
-        'tasks': tasks
+        'tasks': tasks,
     }
     return render(request, 'home.html', context)
