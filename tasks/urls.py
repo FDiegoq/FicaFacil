d171103b0db1f976670d9914ae31d9b45e9c7f39
Diego <fdiegofreire@gmail.com>
2024-10-06 13:52:10 -0300

    ajustes

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home', home, name='home')
+    path('home', home, name='home'),
+    path('create_task', create_task, name='create_task')
 ]
