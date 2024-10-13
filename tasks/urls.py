f04f107be2ac9febf9ec5aace6c63ebcd375ec55
Diego <fdiegofreire@gmail.com>
2024-10-13 18:06:30 -0300

    Está concluindo tarefas com sucesso

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,6 +6,8 @@ from .views import *
 urlpatterns = [
     path('home', home, name='home'),
     path('create_task', create_task, name='create_task'),
+    path('finish_task/<int:id>', finish_task, name='finish_task'),
+    path('done_tasks', done_tasks, name='done_tasks')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
